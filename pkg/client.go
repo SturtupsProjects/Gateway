313dbc97c7f590d99391180b78e9e292da383067
fazliddin <hajrullaevfazliddin058@gmail.com>
2024-12-23 23:04:39 +0500

    host fixed

--- pkg/client.go
+++ pkg/client.go
@@ -12,21 +12,21 @@ import (
 )
 
 func NewUserClient(cfg *config.Config) pbu.AuthServiceClient {
-	conn, err := grpc.NewClient("localhost"+cfg.USER_SERVICE, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.NewClient("crm-admin_auth"+cfg.USER_SERVICE, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("failed to connect to Task Management Service: %v", err)
 	}
 	return pbu.NewAuthServiceClient(conn)
 }
 func NewProductClient(cfg *config.Config) pbp.ProductsClient {
-	conn, err := grpc.NewClient("localhost"+cfg.PRODUCT_SERVICE, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.NewClient("product"+cfg.PRODUCT_SERVICE, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("failed to connect to Task Management Service: %v", err)
 	}
 	return pbp.NewProductsClient(conn)
 }
 func NewCompanyClient(cfg *config.Config) pbc.CompanyServiceClient {
-	conn, err := grpc.NewClient("localhost"+cfg.USER_SERVICE, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.NewClient("crm-admin_auth"+cfg.USER_SERVICE, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("failed to connect to Task Management Service: %v", err)
 	}
